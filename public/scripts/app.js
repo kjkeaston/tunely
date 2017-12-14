c5c0c8b59ce759361d3ad8e7442eca41fe5c46e3
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2017-12-14 15:25:51 -0800

    app.js update to display all hardcoded albums

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -32,10 +32,58 @@ var sampleAlbums = [{
 
 $(document).ready(function() {
   console.log('app.js loaded!');
-});
 
-
-// this function takes a single album and renders it to the page
 function renderAlbum(album) {
-  console.log('rendering album:', album);
+  for (i = 0; i < sampleAlbums.length; i++) {
+    $(".panel-body").append(
+      `<div class='row'>
+          <div class="col-md-3 col-xs-12 thumbnail album-art">
+            <img src="/images/800x800.png" alt="album image">
+          </div>
+
+          <div class="col-md-9 col-xs-12">
+            <ul class="list-group">
+              <li class="list-group-item">
+                <h4 class='inline-header'>Album Name:</h4>
+                <span class='album-name'>${sampleAlbums[i].name}</span>
+              </li>
+
+              <li class="list-group-item">
+                <h4 class='inline-header'>Artist Name:</h4>
+                <span class='artist-name'>${sampleAlbums[i].artistName}</span>
+              </li>
+
+              <li class="list-group-item">
+                <h4 class='inline-header'>Released date:</h4>
+                <span class='album-releaseDate'>${sampleAlbums[i].releaseDate}</span>
+              </li>
+            </ul>
+          </div>
+        </div>`
+    );
+  }
 }
+
+renderAlbum();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////
+// end for document ready function
+});
